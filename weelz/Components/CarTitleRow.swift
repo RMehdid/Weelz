8c8c6fe6f43d48b4d7dfca1de982d3239277654a
Samy Abderraouf Mehdid <rmehdid@harbineldjazair.com>
2024-02-08 10:20:47 +0100

    added car title row

---
+++ weelz/Components/CarTitleRow.swift
@@ -0,0 +1,33 @@
+//
+//  CarTitleRow.swift
+//  weelz
+//
+//  Created by Samy Mehdid on 8/2/2024.
+//
+
+import SwiftUI
+
+struct CarTitleRow: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading){
+                Text("Mercedes")
+                    .font(.system(size: 24, weight: .bold))
+                Text("AMG S63")
+                    .font(.system(size: 16, weight: .regular))
+            }
+            
+            Spacer()
+            
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: 24, height: 24)
+            Text("4.5")
+                .font(.system(size: 20, weight: .regular))
+        }
+    }
+}
+
+#Preview {
+    CarTitleRow()
+}
